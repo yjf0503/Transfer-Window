1a60ba2e3d3064b0a01a1f54e34c226e961c6809
Seven_漆 <1921219336@qq.com>
2017-05-20 18:50:56 +0800

    消息模块

---
+++ pages/messages/messages.js
@@ -0,0 +1,150 @@
+// pages/messages/messages.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0, // tab切换 
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    //获取系统信息 
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+
+    });  
+
+    var resume_ids = '';
+    var resume_id_list = wx.getStorageSync('resume_id_list');
+    var resume_position_id_list = wx.getStorageSync('resume_position_id_list');
+
+    for (var i = 0; i < resume_id_list.length; i++) {
+      resume_ids += resume_id_list[i].id + ",";
+    }
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 1000
+    });
+
+    wx.request({
+      url: 'https://www.ecosports.cn/home/enterprise/wxapp_get_jobsubmitlist',
+      data: { ids: resume_ids },
+      method: 'POST',
+      header: {
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          jobsubmitlist: res.data
+        });
+        wx.setStorageSync('jobsubmitlist', res.data);
+
+      },
+      fail: function () {
+        console.log('服务器请求失败!')
+      },
+    })
+
+    var jobsubmitlist = wx.getStorageSync('jobsubmitlist');
+    that.setData({
+      jobsubmitlist: jobsubmitlist
+    });
+
+    for (var i = 0; i < jobsubmitlist.length; i++) {
+      for (var j = 0; i < resume_id_list.length; i++) {
+        if (resume_id_list[j].id == jobsubmitlist[i].resumeid) {
+          if (resume_id_list[j].status != jobsubmitlist[i].status) {
+            console.log('新');
+            resume_id_list[j].status = jobsubmitlist[i].status;
+            wx.setStorageSync('resume_id_list', resume_id_list);
+          } else {
+            console.log('旧');
+          }
+        }
+      }
+    } 
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var that = this;
+    var resume_id_list = wx.getStorageSync('resume_id_list');
+    var jobsubmitlist = wx.getStorageSync('jobsubmitlist');
+    var resume_position_id_list = wx.getStorageSync('resume_position_id_list');
+
+    that.setData({
+      jobsubmitlist: jobsubmitlist
+    });
+
+    for (var i = 0; i < jobsubmitlist.length; i++) {
+      for (var j = 0; i < resume_id_list.length; i++) {
+        if (resume_id_list[j].id == jobsubmitlist[i].resumeid) {
+          if (resume_id_list[j].status != jobsubmitlist[i].status) {
+            console.log('新');
+            resume_id_list[j].status = jobsubmitlist[i].status;
+            wx.setStorageSync('resume_id_list', resume_id_list);
+          } else {
+            console.log('旧');
+          }
+        }
+      }
+    } 
+  },
+
+  //滑动切换tab
+  bindChangeTab: function (e) {
+    
+    this.setData({ currentTab: e.detail.current });
+
+  },
+  
+  //点击tab切换
+  swichNav: function (e) {
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+
+  //地图
+  mapTap: function(){
+    // wx.getLocation({
+    //   type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+    //   success: function (res) {
+    //     var latitude = res.latitude
+    //     var longitude = res.longitude
+    //     wx.openLocation({
+    //       latitude: latitude,
+    //       longitude: longitude,
+    //       scale: 28
+    //     })
+    //   }
+    // })
+    wx.chooseLocation({
+      success:function(res){
+        consoloe.log(res);
+      }
+    })
+  },  
+})
---
+++ pages/position-detail/position-detail.js
@@ -0,0 +1,98 @@
+// pages/position-detail/position-detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+  //发送简历
+  isSendTap: function(){
+    wx.showModal({
+      title: '发送确认',
+      content: '发送后不可撤回，确认发送？',
+      cancelText: '在考虑下',
+      cancelColor:'#999',
+      confirmText:'立即发送',
+      confirmColor:'#4990E2',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  }
+})
